55afefdd9c602068702897efcb23ff1f5090eb74
Saidurga <67089144+NAGAVENKATASAIDURGA@users.noreply.github.com>
2024-02-19 04:56:47 +0000

    Update Controller.java

--- src/main/java/com/story/application/Controller.java
+++ src/main/java/com/story/application/Controller.java
@@ -3,7 +3,8 @@ package com.story.application;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("/palo")
 public class Controller {
